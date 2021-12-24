63f0bac4539baaafb70c37662776749ad7df8761
Melih Yaşar <mustafa.melih.ysr@outlook.com>
2021-12-24 21:44:41 +0300

    Create yılbaşı sayacı.py

---
+++ yılbaşı sayacı.py
@@ -0,0 +1,11 @@
+import datetime
+import time
+while 1:
+    now = datetime.datetime.now()
+    kalan_zaman = datetime.datetime(now.year + 1, 1, 1) - now
+    saat= kalan_zaman.seconds//3600
+    dakika = (kalan_zaman.seconds%3600) //60
+    saniye = (kalan_zaman.seconds%3600) %60
+
+    print("Yeni yıla kalan zaman: " + str(kalan_zaman.days) + " gün,"+ str(saat) + " saat,"+ str(dakika) + " dakika," + str(saniye) + " saniye.", end = "\r")
+    time.sleep(0.5)
